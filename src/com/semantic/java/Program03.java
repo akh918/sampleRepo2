d179c5d0f1096e750a1cd42c2f96ea062a6e3ea5
raza <raza@gmail.com>
2019-11-13 13:50:05 +0530

    conflict Commit

--- src/com/semantic/java/Program03.java
+++ src/com/semantic/java/Program03.java
@@ -7,5 +7,6 @@ public class Program03 {
     public static  void main(String[] args){
         System.out.println(4+5-8);
         System.out.println(10/3);
+        System.out.println(10-3);
     }
 }
