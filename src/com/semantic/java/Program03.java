befb87ee90f2fa21706b6fc446150b5692fe524a
SandeepCherukuri <sandeep.cherukuri@india.semanticbits.com>
2019-11-13 14:10:25 +0530

    " subha commit"

--- src/com/semantic/java/Program03.java
+++ src/com/semantic/java/Program03.java
@@ -7,6 +7,7 @@ public class Program03 {
     public static  void main(String[] args){
         System.out.println(4+5-8);
         System.out.println(10/3);
-        System.out.println(10-3);
+
+
     }
 }
