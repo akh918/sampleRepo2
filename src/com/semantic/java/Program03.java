97ed914f174d29109858703ce7fbb05a7bd4836a
ac <praveen.pavan0509@gmail.com>
2019-11-13 14:06:35 +0530

    new modifie in program03 by akhilesh

--- src/com/semantic/java/Program03.java
+++ src/com/semantic/java/Program03.java
@@ -10,5 +10,6 @@ public class Program03 {
         System.out.println(10-3);
         System.out.println(55);
         System.out.println("jkdfndjksnfjkdjk");
+        System.out.print("alkhilesh");
     }
 }
