66bd7244b3123796f1528c2f4933e68605538e34
raza <raza@gmail.com>
2019-11-13 12:46:50 +0530

    Second Commit

--- src/com/semantic/java/Program01.java
+++ src/com/semantic/java/Program01.java
@@ -3,6 +3,7 @@ package com.semantic.java;
 public class Program01 {
 
     public static void main(String[] args) {
+        System.out.println("Anas");
         System.out.println("Raza");
     }
 }
